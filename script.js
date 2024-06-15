3c5c7301434b09344431c98068d0bfc575f15389
quelvaz <raraa.brito@gmail.com>
2024-06-14 21:57:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function limparConsulta() {
     document.getElementById('nomeConsulta').value = '';  // Limpa o campo de consulta
     document.getElementById('listaFuncionarios').innerHTML = '';  // Limpa a lista de resultados
 }
+
+function limparConsultaRegistro() {
+    document.getElementById('dataConsulta').value = '';  // Limpa o campo de consulta
+    document.getElementById('listaPontos').innerHTML = '';  // Limpa a lista de resultados
+}
